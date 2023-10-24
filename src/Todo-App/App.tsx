a790742fa27fb229a8a91fa585d92864f01ec7d7
CodoFile <alamf6023@gmail.com>
2023-10-24 23:59:58 +0530

    added browser router

--- src/Todo-App/App.tsx
+++ src/Todo-App/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter } from "react-router-dom"
+import AddTodo from "./components/AddTodo"
 import Navbar from "./components/Navbar"
+import Todos from "./components/Todos"
+import { TodosProvider } from "./store/todos"
 
 function App() {
     return (
-      <div className="min-h-[100vh] max-h-[100%] bg-blue-950 flex flex-col selection:text-blue-600 selection:underline items-center">
-        <h1 className="text-white uppercase text-3xl mt-5 font-serif">Todo List</h1>
-        <Navbar/>
-      </div>
+        <BrowserRouter>
+        <TodosProvider>
+            <div className="min-h-[100vh] max-h-[100%] bg-blue-950 flex flex-col selection:text-blue-600 selection:underline items-center">
+                <h1 className="text-white uppercase text-3xl mt-5 font-serif">Todo List</h1>
+                <Navbar/>
+                <AddTodo/>
+                <Todos/>
+            </div>
+        </TodosProvider>
+        </BrowserRouter>
     )
   }
   
