15b6c77345182dbbf1d19512f922d789299ad543
CodoFile <alamf6023@gmail.com>
2023-10-25 10:49:11 +0530

    minor improvement

--- src/Todo-App/App.tsx
+++ src/Todo-App/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <BrowserRouter>
         <TodosProvider>
             <div className="min-h-[100vh] max-h-[100%] bg-blue-950 flex flex-col selection:text-blue-600 selection:underline items-center">
-                <h1 className="text-white uppercase text-xl mt-5 font-serif">Todo List</h1>
+                <h1 className="text-white uppercase text-xl lg:text-3xl mt-5 font-serif">Todo List</h1>
                 <Navbar/>
                 <AddTodo/>
                 <Todos/>
