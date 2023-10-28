7d5dff8eeb9d62f2780d74721da91d32dd3dd418
CodoFile <alamf6023@gmail.com>
2023-10-28 16:49:51 +0530

    Toaster Added

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,20 @@ import TodoApp from './Todo-App/App' ;
 import HomePage from './home-page/App';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Toaster} from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Toaster
+          position='top-center'
+          toastOptions={{
+            success:{
+              theme:{
+                primary:'#4aed88'
+              }
+            }
+          }}
+        />
     <BrowserRouter>
       <Routes>
         <Route path='/todo' element={<TodoApp/>} />
